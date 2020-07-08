da9735fab873a942bf96454e9770c0832a7d3f0d
Tuan nguyen <tuan.nguyen@nodeplusplus.com>
2020-07-08 10:38:43 +0700

    fix missing function

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as mongodb from "./mongodb";
 import * as redis from "./redis";
 import * as serializer from "./serializer";
+import { merge } from "./merge";
 
 export * from "./types";
-export default { mongodb, redis, serializer };
+export default { mongodb, redis, serializer, merge };
